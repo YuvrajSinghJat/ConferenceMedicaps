31b55cbb15d63421677865964491ec1d886416c4
Yuvraj Singh Jat <yuvrajsingh1008jat@gmail.com>
2025-05-30 00:36:45 +0530

    Image tag is fixed

--- src/pages/AboutMedicaps/AboutMedicaps.js
+++ src/pages/AboutMedicaps/AboutMedicaps.js
@@ -25,7 +25,7 @@
                                 <h1 className='sm:text-4xl text-xl font-black text-white sm:ml-12 bodyFont2'>ABOUT MEDI-CAPS UNIVERSITY
                                 </h1>
           
-                                <img className='ml-3' src={"https://www.medicaps.ac.in/public/frontend/images/footer-logo.webp"} alt="" width={100} /
+                                <img className='ml-3' src={"https://www.medicaps.ac.in/public/frontend/images/footer-logo.webp"} alt="" width={100} />
                             </div>
 
                             {/* IMAGE SECTION */}
